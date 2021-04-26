c1d5b1f8fca40fe0ea5a870b21f1a7ed8b8c5c52
FGHII <eric.j.klosterman@gmail.com>
2021-04-26 13:59:02 -0500

    created palindromes algorithm

--- palindromes.js
+++ palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+  string = string.toLowerCase();
+  const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g;
+  string = string.replace(regex, '');
+  console.log(string);
+  let palindromeFlag = true;
+  let spaceBetween = string.length - 2;
+  let stringStart = 0;
+  let stringEnd = string.length - 1;
+  while (spaceBetween >= 1) {
+  if (string.charAt(stringStart) == string.charAt(stringEnd)) {
+    console.log("Space between: " + spaceBetween);
+    console.log("match");
+    console.log(string.charAt(stringStart) + "," + string.charAt(stringEnd));
+    stringStart++;
+    stringEnd--;
+    spaceBetween -= 2;
+  } else {
+    console.log("not a match");
+    console.log(string.charAt(stringStart) + "," + string.charAt(stringEnd));
+    spaceBetween = 0;
+    palindromeFlag = false;
+  }
+  console.log(palindromeFlag);
+}
 }
 
-module.exports = palindromes
+let string = prompt("Enter a string:")
+palindromes(string);
+
+
+// module.exports = palindromes
